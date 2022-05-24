e849c8cfda8a23b4efc84f4595d31c22abcbb391
Janette Campbell <campbjp@hotmail.com>
2022-05-23 22:03:55 -0400

    added new student page

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// home page
 app.get("/", (req, res) => {
   res.render("home", {
     pageTitle: "Student Home Page",
@@ -24,6 +25,9 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/events", (req, res) => {});
+
+// list of all students
 app.get("/students", (req, res) => {
   res.render("students", {
     data: studentsData,
@@ -32,6 +36,22 @@ app.get("/students", (req, res) => {
   });
 });
 
+// new student form
+app.get("/students/new", (req, res) => {
+  res.render("new-students", {
+    pageTitle: "New Student",
+    headerTitle: "Add New Student",
+  });
+});
+
+// create new student
+app.post("/students", (req, res) => {
+  console.log(req.body);
+  studentsData.push(req.body);
+  res.redirect("/students");
+});
+
+// search by student ID
 app.get("/students/:id", (req, res) => {
   const result = students.filter(
     (student) => student.id === Number(req.params.id)
@@ -39,6 +59,7 @@ app.get("/students/:id", (req, res) => {
   res.send(result);
 });
 
+// search by full name
 app.get("/students/search/:name", (req, res) => {
   const result = students.filter((student) => student.name === req.params.name);
   res.send(result);
